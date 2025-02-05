ff524f1f26aea025714460f9046360fce4219b4b
N C <6445377+nicholaschenai@users.noreply.github.com>
2025-02-05 15:52:53 +0800

    remove old checkpoints as settings not the same as paper

--- custom_checkpoints/eureka/2025-01-19_21-49-31/env_iter0_response1_rewardonly.py
+++
@@ -1,9 +0,0 @@
-@torch.jit.script
-def quat_mul(q1: torch.Tensor, q2: torch.Tensor) -> torch.Tensor:
-    """Quaternion multiplication."""
-    w1, x1, y1, z1 = q1.unbind(-1)
-    w2, x2, y2, z2 = q2.unbind(-1)
-    return torch.stack((w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2,
-                        w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2,
-                        w1 * y2 - x1 * z2 + y1 * w2 + z1 * x2,
-                        w1 * z2 + x1 * y2 - y1 * x2 + z1 * w2), dim=-1)
